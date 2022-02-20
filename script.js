9e4537e8388a4dcda8d4020b040fcf078902f2d8
LevAverkiev <levaverkiev@gmail.com>
2022-02-20 22:10:32 +0300

    Сделал все задания

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const btn = document.getElementById("btn");
+const eBtn = document.getElementById("e_btn");
+const square = document.getElementById("square");
+const circle = document.getElementById("circle");
+const text = document.getElementById("text");
+const range = document.getElementById("range");
+const span = document.getElementById("range-span");
+eBtn.style.display = "none";
+
+const logger = function () {
+    square.style.backgroundColor = text.value;
+};
+
+const logger2 = function () {
+    span.textContent = range.value + " %";
+
+    circle.style.width = range.value + "%";
+    circle.style.height = range.value + "%";
+};
+
+btn.addEventListener("click", logger);
+range.addEventListener("input", logger2);
+range.addEventListener("change", logger2);
